d36da394674d53204d56c29d76a7526357cc3239
server <server@server.com>
2017-07-17 17:17:14 -0400

    Added Destroy method and refactoring of the code to remove redundancies

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-  
+  before_action :set_todo, only: [:edit, :update, :show, :destroy ]
   def new
     @todo = Todo.new
   end
@@ -15,15 +15,12 @@ class TodosController < ApplicationController
   end
   
   def show
-    @todo = Todo.find(params[:id])
   end
 
   def edit
-    @todo = Todo.find(params[:id])
   end
   
   def update
-    @todo = Todo.find(params[:id])
     if @todo.update(todo_params)
       flash[:notice] = "Update successfully saved"
       redirect_to todo_path(@todo)
@@ -36,7 +33,16 @@ class TodosController < ApplicationController
     @todos = Todo.all 
   end
 
+  def destroy
+    @todo.destroy
+    flash[:notice] = "Todo item was sucessfully deleted"
+    redirect_to todos_path 
+  end
+
   private
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
 
   def todo_params
     params.require(:todo).permit(:name, :description)
